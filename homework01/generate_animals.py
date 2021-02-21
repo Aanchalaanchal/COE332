17a471032e068745acc16558a8bf112d4a0064ee
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2021-02-21 12:20:41 -0600

    Create generate_animals.py

---
+++ homework01/generate_animals.py
@@ -0,0 +1,23 @@
+import random
+import json
+import petname
+
+given_animals={"animals":[]}
+given_list=['snake', 'bull', 'lion', 'raven', 'bunny']
+for _ in range(20):
+   head=random.choice(given_list)
+   animalA=random.choice(petname.names)
+   animalB=random.choice(petname.names)
+   animal_name=animalA+'-'+animalB
+   arms=random.randint(2,10)
+   legs=random.randint(3,12)
+   tails=arms+legs
+   new_animal={"head": head,
+                "body": animal_name,
+                "arms": arms,
+                "legs": legs,
+                "tail": tails}
+   given_animals['animals'].append(new_animal)
+
+   with open("animals.json", "w") as outfile:
+       json.dump(given_animals, outfile)

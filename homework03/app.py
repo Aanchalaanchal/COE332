865d96e202ff01da9ad86356bbb7f72b5e5bfd0a
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2021-04-05 13:45:20 -0500

    Create app.py

---
+++ homework03/app.py
@@ -0,0 +1,34 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def get_data():
+    with open("animals.json", "r") as json_file:
+        userdata = json.load(json_file)
+    return userdata
+test = get_data()
+print (type(test))
+jsonList = test['animals']
+print (type(jsonList))
+output1 = [x for x in jsonList if x['head'] == 'snake']
+print (output1)
+
+
+@app.route('/animals')
+def getanimals():
+        new_output = [x for x in jsonList]
+        print (new_output)
+
+@app.route('/animals/heads/bunny')
+def getheads():
+        output2 = [x for x in jsonList if x['head'] == 'bunny']
+        print (output2)
+
+@app.route('/animals/legs/6')
+def getlegs():
+        output3 = [x for x in jsonList if x['legs'] == 6]
+        print (output3)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

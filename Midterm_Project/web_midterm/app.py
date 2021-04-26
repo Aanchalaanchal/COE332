d1bcb50902f8c741e55281c5138bbe8d695e5a41
Aanchalaanchal <79412247+Aanchalaanchal@users.noreply.github.com>
2021-04-25 23:38:58 -0500

    Update app.py

--- Midterm_Project/web_midterm/app.py
+++ Midterm_Project/web_midterm/app.py
@@ -15,6 +15,42 @@ print (type(jsonList))
 output1 = [x for x in jsonList if x['head'] == 'snake']
 print (output1)
 
+#def get_data():
+#    rd = redis.StrictRedis(host='redis', port=6379, db=8)
+#    userdata = {"animals":[]}
+#    for x in range(100):
+#        new_animal={"head": head,
+#                     "body": animal_name,
+#                     "arms": arms,
+#                     "legs": legs,
+#                     "tail": tails,
+#                     "uid" : str(uuid.uuid4()),
+#                     "created on" : str(datetime.datetime.now())}
+#        userdata['animals'].append(animal)
+
+@app.route('/establish', methods=['GET'])
+def establish_database():
+    rd = redis.StrictRedis(host='redis', port=6379, db=8)
+    given_animals={"animals":[]}
+    given_list=['snake', 'bull', 'lion', 'raven', 'bunny']
+    for _ in range(100):
+       head=random.choice(given_list)
+       animalA=random.choice(petname.names)
+       animalB=random.choice(petname.names)
+       animal_name=animalA+'-'+animalB
+       arms=random.randrange(2,11,2)
+       legs=random.randrange(3,13,3)
+       tails=arms+legs
+       new_animal={"head": head,
+                    "body": animal_name,
+                    "arms": arms,
+                    "legs": legs,
+                    "tail": tails,
+                    "uid" : str(uuid.uuid4()),
+                    "created on" : str(datetime.datetime.now())}
+       rd.hmset(x, new_animal)
+    return "database created"
+
 
 @app.route('/animals', methods=['GET'])
 def getanimals():
